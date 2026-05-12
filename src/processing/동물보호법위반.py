0ff7f17d1a1d25fab47ab3a563230eaa70247863
dokyung253-lang <dokyung253@gmail.com>
2026-05-12 14:40:35 +0900

    fix : 동물보호법위반.py 전처리 수정

--- src/processing/동물보호법위반.py
+++ src/processing/동물보호법위반.py
@@ -1,27 +1,27 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import korean_font
+import pandas as pd
+import matplotlib.pyplot as plt
+import korean_font
 
-# # csv 파일 읽기
-# df = pd.read_csv(
-#     'data/raw_data/crime(23-24).csv',
-#     header=0,
-#     encoding='CP949'
-# )
+# csv 파일 읽기
+df = pd.read_csv(
+    'data/raw_data/동물보호법(23-24).csv',
+    header=0,
+    encoding='CP949'
+)
 
-# # 형변환
-# for col in ['2023', '2024']:
-#     df[col] = pd.to_numeric(df[col], errors='coerce')
+# 형변환
+for col in ['2023', '2024']:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
 
-# print(df.dtypes)
-# print(df)
+print(df.dtypes)
+print(df)
 
-# # 1. .csv 내보내기
-# df.to_csv(
-#     './day14/data_out.csv',  # 파일경로
-#     index = False,           # 인덱스 제외
-#     encoding='utf-8',        # 인코딩 지정
-#     na_rep='U',              # 결측값 치환
-#     header= True             # 헤더(열이름) 포함 여부
-# )
+# 1. .csv 내보내기
+df.to_csv(
+   'data/raw_data/동물보호법(23-24).csv',  # 파일경로
+    index = False,           # 인덱스 제외
+    encoding='utf-8',        # 인코딩 지정
+    na_rep='U',              # 결측값 치환
+    header= True             # 헤더(열이름) 포함 여부
+)
 
