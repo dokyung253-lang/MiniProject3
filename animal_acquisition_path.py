1500099a0d46df836342741c12a4b26d36c190fd
leehyeon2021 <leehyeon2021@gmail.com>
2026-05-12 15:54:16 +0900

    chore: 동물등록률 수정, 이태현

--- animal_acquisition_path.py
+++ animal_acquisition_path.py
@@ -4,7 +4,8 @@ import korean_font
 # [1. 동물 취득 경로 데이터 전처리]
 df = pd.read_csv('pet_shop_acquisition.csv', encoding='utf-8')
 print( df.head() )
-
+df.info()
+print( df.isnull().sum() )
 
 # 1) 연도 컬럼 부여
 #df['연도'] = 
--- animal_registration_rate.py
+++ animal_registration_rate.py
@@ -2,9 +2,6 @@ import pandas as pd
 import korean_font
 
 # =====
-# << 주의 >>
-# '서울가구수'는 동물등록률과 관련 없이 서울 자치구 당 전체 가구 수를 의미합니다.
-# =====
 
 # [1. 동물등록 현황 데이터 전처리 및 병합]
 df_23 = pd.read_csv('2023년 서울시 동물등록 현황.csv', encoding='utf-8')
@@ -24,39 +21,27 @@ for col in number_cols:
 
 # =====
 
-# [2. 가구수 데이터 평탄화 전처리]
-df_h = pd.read_csv('seoul_household.csv', skiprows=5, header=None, encoding='utf-8')
+# [2. 파생변수: 동물 등록 증가율]
+# 1) 분모: 24년 합계 - 23년 합계
+df_23_total = df_pet[(df_pet['자치구']=='합계') & (df_pet['연도']==2023)]['계'].values[0]   # 622137
+df_24_total = df_pet[(df_pet['자치구']=='합계') & (df_pet['연도']==2024)]['계'].values[0]
+df_demo = df_24_total - df_23_total
+# '합계' 퇴장
+df_pet = df_pet[df_pet['자치구'] != '합계'].copy()
 
-# 1) 2023 가구수 데이터 새로 만들기
-df_h_23 = df_h[ [1, 2] ].copy()
-df_h_23.columns = ['자치구', '서울가구수']
-df_h_23['연도'] = 2023
-#print(df_h)
+# 2) 23년도 거 계산 쉽게 '작년계' 만들기
+df_dict = df_pet[df_pet['연도'] == 2023].set_index('자치구')['계'].to_dict()
+df_pet['작년계'] = df_pet['자치구'].map(df_dict)
 
-# 2) 2024 가구수 데이터 새로 만들기
-df_h_24 = df_h[ [1, 22] ].copy()
-df_h_24.columns = ['자치구', '서울가구수']
-df_h_24['연도'] = 2024
+# 3) 증가율: (올해 계 - 작년 계) / 서울시 전체 증가량 * 100
+df_pet['등록증가율(%)'] = (( df_pet['계'] - df_pet['작년계'] ) / df_demo ) * 100
 
-# 3) 가구수 데이터 통합
-df_household = pd.concat( [df_h_23, df_h_24] , ignore_index=True )
-df_household['자치구'] = df_household['자치구'].str.strip()
-df_household = df_household[ ~df_household['자치구'].isin( ['소계'] ) ]
-
-# 4) 가구수 수치 형변환
-df_household['서울가구수'] = pd.to_numeric(df_household['서울가구수'].astype(str).str.replace(',', ''), errors='coerce')
-#print(df_household)
+# 4) '작년계' 지우기
+df_pet = df_pet.drop(columns=['작년_계'])
 
 # =====
 
-# [3. 최종 테이블 (등록률 포함시킨 것)]
-# 1) 자치구와 연도 기준 -> 테이블 하나로 병합
-df_final = pd.merge( df_pet , df_household , on=['자치구', '연도'], how='inner' )
-
-# 2) 파생변수
-df_final['동물등록률'] = ( df_final['계'] / df_final['서울가구수'] ) * 100
-
-# print(df_final.head())
-# print(f"동물등록 데이터 행 개수: {len(df_pet)}")
-# print(f"최종 병합 데이터 행 개수: {len(df_final)}")
-df_final.info()
+# [3. 확인]
+df_pet.info()
+print( df_pet.head() )
+print( df_pet.isnull().sum() )
