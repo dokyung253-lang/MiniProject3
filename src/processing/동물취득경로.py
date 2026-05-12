7ae9dae68408354a8baddc99918d228c929e3b6b
so0hub <so0mile02@gmail.com>
2026-05-12 18:08:50 +0900

    fix: 동물취득경로.py 오타수정 박소영

--- src/processing/동물취득경로.py
+++ src/processing/동물취득경로.py
@@ -14,28 +14,17 @@ print( df )
 
 col_names = ['자치구별', '펫샵구매', '친척친구_무료', '친척친구_유료', '인터넷구매', '유기동물입양', '반려동물의 자식', '기타']
 
-# 23 펫샵구매+인터넷구매
-buy_2023 = df.iloc[ : , [1] + list(range(2,9)) ].copy()
-buy_2023.columns = col_names
-for col in col_names[1: ] :
-    buy_2023[col] = pd.to_numeric( buy_2023[col] , errors='coerce' ).fillna(0)
-
-buy_2023['펫샵_구매'] = buy_2023['펫샵구매'] + buy_2023['인터넷구매']
-buy_2023['연도'] = 2023
-
-
 # 24
 buy_2024 = df.iloc[ : , [1] + list(range(9,16)) ].copy()
 buy_2024.columns = col_names
+
 for col in col_names[1: ] :
     buy_2024[col] = pd.to_numeric( buy_2024[col] , errors='coerce' ).fillna(0)
+
 buy_2024['펫샵_구매'] = buy_2024['펫샵구매'] + buy_2024['인터넷구매']
 buy_2024['연도'] = 2024
 
-
-
-buy_final = pd.concat([buy_2023,buy_2024],ignore_index=True)
-buy_final = buy_final[~buy_final['자치구별'].str.contains('구분별|지역소분류', na=False)]
+buy_final = buy_2024[~buy_2024['자치구별'].str.contains('구분별|지역소분류', na=False)]
 
 
 print( buy_final )
