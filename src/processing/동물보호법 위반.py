84c5aed38541c6098c5ddae165443b03310dba15
dokyung253-lang <dokyung253@gmail.com>
2026-05-12 17:18:40 +0900

    feat : 증감률 추가, 임도경

--- src/processing/동물보호법 위반.py
+++ src/processing/동물보호법 위반.py
@@ -14,3 +14,6 @@ for col in ['2023', '2024']:
     df[col] = pd.to_numeric(df[col], errors='coerce')
 print(df)
 
+# 파생변수 생성
+df['증감률_%'] = (df['2024'] - df['2023']) / df['2023'] * 100
+print (df['증감률_%'])
