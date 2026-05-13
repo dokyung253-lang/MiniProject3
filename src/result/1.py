99150669c4dcd08072f90efca49f821d02495008
dokyung253-lang <dokyung253@gmail.com>
2026-05-13 17:29:17 +0900

    feat : 가설 1 완료 , 임도경

---
+++ src/result/1.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import korean_font  # 한글 폰트 설정 모듈
+
+# CSV 불러오기
+df = pd.read_csv('data/반환상태크롤링.csv')
+
+# 등록번호 존재 여부를 True/False로 변환
+df['등록여부'] = df['등록번호'].notna()
+
+# True/False 개수 세기
+counts = df['등록여부'].value_counts()
+
+# 원형 차트 그리기
+plt.figure(figsize=(6,6))
+counts.plot(
+    kind='pie',
+    autopct='%1.1f%%',
+    startangle=90,
+    colors=['skyblue','salmon'],
+    labels=['등록', '미등록']
+)
+plt.title("반환된 반려동물 중 등록/미등록 비율")
+plt.ylabel("")  # y축 라벨 제거
+plt.show()
